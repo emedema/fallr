17c78a5bdd21011cde293f02cac09c60389d0e5f
Nathan Nesbitt <nathan@nesbitt.ca>
2020-01-29 20:51:43 -0800

    Created sample functions for like and follow

--- Src/user.php
+++ Src/user.php
@@ -25,27 +25,12 @@
                         <p>Followers</p>
                         <form id="follow-form">
                             <input hidden name="username" value="<?php echo($_GET["username"]);?>">
-                            <input id="follow-button" type="button" value="Follow"></input>
+                            <button id="follow-button" type="button">Subscribe</button>
                         </form>
                     </div>
                 </div>
-                <div>
-                    <!-- This is an individual post, which can be duplicated n times -->
-                    <section>
-                        <!-- This is the 'head of the post' which includes the title and the image -->
-                        <div>
-                            <div>
-                                <img>
-                            </div>
-                            <div>
-                                <p>Title of the blog post.</p>
-                            </div>
-                        </div>
-                        <!-- This is the content in the post -->
-                        <div>
-                            <p>This is the content of my post.</p>
-                        </div>                        
-                    </section>
+                <div id="body">
+                    
                 </div>
             </div>
         </body>
@@ -62,17 +47,74 @@
         <script src="https://cdn.jsdelivr.net/npm/js-cookie@beta/dist/js.cookie.min.js"></script>
         
         <script>
+            $( document ).ready(
+                $.ajax({
+                    url: 'http://localhost/fallrAPI/feed/',
+                    type: 'POST',
+                    dataType: 'JSON',
+                    data: "userID=" + Cookies.get("loggedIn").split(":")[0],
+                    success: function(data) {
+                        var newPosts = "";
+                        $.each(data, function(key, value){
+                            newPosts += "<section><form><input type='hidden' name='postID' class='postID' value='" + value.postID + "'>";
+                            newPosts += "<div><div><img></div>";
+                            newPosts += "<div><p>" + value.postName + "</p></div></div>";
+                            newPosts += "<div><p>" + value.postContent + "</p></div>";
+                            newPosts += "<div><button class='like-button' type='button'>Like</button><button class='view-comments-button' type='button'>View Comments</button></div></form></section>";
+                        });
+                        $('#body').html(newPosts);
+                    }
+                }),
+            );
+
             $("#follow-button").click(function() {
                 $.ajax({
                     url: 'http://localhost/fallrAPI/subscribe',
                     type: 'POST',
-                    dataType: 'JSON',
-                    data: $('#follow-form').serialize(),
+                    dataType:'text',
+                    data: $('form').serialize(),
                     success: function(data) {
-                        console.log("success");
+                        if(data == 1)
+                            $("#follow-button").html("Unsubscribe");
+                        else
+                            $("#follow-button").html("Subscribe");
                     }
                 });
-            }); 
+            });
+
+            $("#body").on('click','.like-button', function(event){
+                // This selects the parent form for the button and submits the like for it //
+                var button = this;
+                $.ajax({
+                    url: 'http://localhost/fallrAPI/likes',
+                    type: 'POST',
+                    dataType:'text',
+                    data: 'postID=' + $(button).closest("form").find(".postID").val(),
+                    success: function(data) {
+                        if(data == 1)
+                            $(button).html("Unlike");
+                        else
+                            $(button).html("Like");
+                    }
+                });
+            });
+
+            $("#body").on('click','.view-comments-button', function(event){
+                // This selects the parent form for the button and submits the like for it //
+                var button = this;
+                $.ajax({
+                    url: 'http://localhost/fallrAPI/comments',
+                    type: 'POST',
+                    dataType:'text',
+                    data: 'postID=' + $(button).closest("form").find(".postID").val(),
+                    success: function(data) {
+                        if(data == 1)
+                            $(button).html("Unlike");
+                        else
+                            $(button).html("Like");
+                    }
+                });
+            });
         </script>
     </body>
 </html>
