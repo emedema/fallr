4fa3b84fa4f350cee6ea9e08d765c7caa5223ad6
Nathan Nesbitt <nathan@nesbitt.ca>
2020-01-31 13:38:13 -0800

    Added Update and Get posts

--- API/Post.php
+++ API/Post.php
@@ -13,6 +13,28 @@ class Post {
 		/* Returns inserted_id */
 		return $query->insert_id;
     }
+
+    public function updatePost($connection, $postID, $postName, $postContent) {
+	
+		/* Prepares the function so we can pass in the values from the user */
+		$query = $connection->prepare("UPDATE Posts SET postName = ?, postContent = ? WHERE postID= ?");
+		/* Passes the values into the query */
+		$query->bind_param("ssi", $postName, $postContent, $postID);
+        
+		/* Returns Success */
+		return $query->execute();
+    }
+
+    public function deletePost($connection, $postID) {
+	
+		/* Prepares the function so we can pass in the values from the user */
+		$query = $connection->prepare("DELETE FROM Posts WHERE postID = ?");
+		/* Passes the values into the query */
+		$query->bind_param("i", $postID);
+        
+		/* Returns success */
+		return $query->execute();
+    }
     
     public function getHot($connection) {
         /* Prepares the function so we can pass in the values from the user */
@@ -36,6 +58,42 @@ class Post {
         
 		/* Returns inserted_id */
 		return $query->get_result();
+    }
+
+    public function getPosts($connection, $username) {
+        /* Prepares the function so we can pass in the values from the user */
+       $query = $connection->prepare("SELECT postID, username, postName, postContent FROM Posts WHERE username = ?");		
+
+       $query->bind_param("s", $username);
+
+       $query->execute();
+       
+       /* Returns inserted_id */
+       return $query->get_result();
+    }
+
+    public function getPost($connection, $postID) {
+        /* Prepares the function so we can pass in the values from the user */
+       $query = $connection->prepare("SELECT postID, username, postName, postContent FROM Posts WHERE postID = ?");		
+
+       $query->bind_param("i", $postID);
+
+       $query->execute();
+       
+       /* Returns inserted_id */
+       return $query->get_result();
+    }
+
+    public function getPostOwner($connection, $postID) {
+        /* Prepares the function so we can pass in the values from the user */
+        $query = $connection->prepare("SELECT username FROM Posts WHERE postID = ?");		
+
+        $query->bind_param("i", $postID);
+
+        $query->execute();
+        
+        /* Returns inserted_id */
+        return $query->get_result()["username"];
     }
 }
 
